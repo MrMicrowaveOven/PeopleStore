b524a0676ec1139d7340df81c1b1b1ce86d3f302
Benjamin <benj@zagorski.com>
2017-06-11 03:11:53 -0700

    Added AUTH_TOKEN to heading requirements

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,69 +1,83 @@
 class PeopleController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def index
-    if params[:name]
-      @person = Person.find_by_name(params[:name])
-      if @person
-        number_array = @person[:numbers].split
-        render json: number_array
-        return
-      else
-        render json: {error: "#{params[:name]} not found."}
-        return
-      end
+  def check_auth
+    if request.headers["HTTP_AUTH_TOKEN"] != ENV["GAVINS_AUTH_TOKEN"]
+      render json: {authentication_error: "Please include your `AUTH_TOKEN` in the header."}
+      false
     else
-      people = Person.all
-      skinny_people = []
-      people.each do |person|
-        skinny_people.push({
-          name: person.name,
-          numbers: person.numbers.split,
-          created_at: person.created_at,
-          updated_at: person.updated_at
-        })
+      true
+    end
+  end
+
+  def index
+    if check_auth
+      if params[:name]
+        @person = Person.find_by_name(params[:name])
+        if @person
+          number_array = @person[:numbers].split
+          render json: number_array
+          return
+        else
+          render json: {error: "#{params[:name]} not found."}
+          return
+        end
+      else
+        people = Person.all
+        skinny_people = []
+        people.each do |person|
+          skinny_people.push({
+            name: person.name,
+            numbers: person.numbers.split,
+            created_at: person.created_at,
+            updated_at: person.updated_at
+          })
+        end
+        full_get = {
+          people: skinny_people,
+          instructions:
+          "Are you trying to find numbers for a specific person?  If so, please include a `name:` parameter in your request.",
+        }
+        render json: full_get
+        return
       end
-      full_get = {
-        people: skinny_people,
-        instructions:
-        "Are you trying to find numbers for a specific person?
-        If so, please include a `name:` parameter in your request.",
-      }
-      render json: full_get
-      return
     end
   end
 
   def create
-    @person = Person.create_slash_update(person_params)
-    if @person.save
-      number_array = @person[:numbers].split
-      name = person_params[:name]
-      possessive_name = name.chars.last == "s" ? name + "'" : name + "'s"
-      render json: {"#{possessive_name} numbers": number_array}
-    else
-      render json: {error: "Please include both `name:` and `numbers:` parameters when posting."}
+    if check_auth
+      @person = Person.create_slash_update(person_params)
+      if @person.save
+        number_array = @person[:numbers].split
+        name = person_params[:name]
+        possessive_name = name.chars.last == "s" ? name + "'" : name + "'s"
+        render json: {"#{possessive_name} numbers": number_array}
+      else
+        render json: {error: "Please include both `name:` and `numbers:` parameters when posting."}
+      end
     end
   end
 
   def destroy
-    if params[:name]
-      @person = Person.find_by_name(params[:name])
-      if @person
-        if params[:wantToDelete] == "yes"
-          @person.destroy
-          render json: {person: "#{params[:name]} has been destroyed"}
-          return
+    if check_auth
+      if params[:name]
+        @person = Person.find_by_name(params[:name])
+        if @person
+          if params[:wantToDelete] == "yes"
+            @person.destroy
+            render json: {person: "#{params[:name]} has been destroyed"}
+            return
+          else
+            render json: {error: "In order to delete #{params[:name]}, you must include a `wantToDelete: 'yes'` parameter."}
+            return
+          end
         else
-          render json: {error: "In order to delete #{params[:name]}, you must include a `wantToDelete: 'yes'` parameter."}
+          render json: {error: "#{params[:name]} not found."}
           return
         end
       else
-        render json: {error: "#{params[:name]} not found."}
-        return
+        destroy_all(params)
       end
-    else
-      destroy_all(params)
     end
   end
 
